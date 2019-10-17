df89eadd49b4863638602d896a6cf4800dfcc673
James Chapman <chapmanjamesg@gmail.com>
2019-10-17 14:47:18 -0500

    finished adding the dom munipulation

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,14 +1,34 @@
-const journalEntry =  {
-    Date: 10-14-19,
-    concept: 'building DOM Components',
-    entry: 'Today we learned to manipulate the DOM using JAVASCRIPT. later in the day we went over JSON',
-    mood: 'glad'
-};
+const journalEntry = [
+    {
+        date: '10-14-19',
+        concept: 'building DOM Components',
+        entry: 'Today we learned to manipulate the DOM using JAVASCRIPT. later in the day we went over JSON',
+        mood: 'glad'
+    }
+];
 
-const journalUpload = [];
-const journalEntry2= {date:'10-11-19', concept:'objects as things', entry:'today we went over how to manipulate objects in javascript', mood:'confused'};
-const journalEntry3={date:'10-10-19', concept:'functions and logic', entry:'today we went over using functions and methods in javascript', mood:'glad'};
+journalEntry.push({ date: '10-11-19', concept: 'objects as things', entry: 'today we went over how to manipulate objects in javascript', mood: 'confused' });
+journalEntry.push({ date: '10-10-19', concept: 'functions and logic', entry: 'today we went over using functions and methods in javascript', mood: 'glad' });
 
-journalUpload.push(journalEntry, journalEntry2,journalEntry3);
+const makeJournalEntryComponent = (journalEntry) => {
+    return `
+        <h2>${journalEntry.concept}</h2>
+            <div>${journalEntry.date}</div>
+            <aside>${journalEntry.entry}</aside>
+            <aside>${journalEntry.mood}</aside>    
+    `
+}
 
-console.log(journalUpload)
+const renderJournalEntries = (entries) => {
+    let entriesContainer = document.querySelector(".entryLog")
+
+    for (let i = 0; i < journalEntry.length; i++) {
+        entries = journalEntry[i]
+        entriesContainer.innerHTML += makeJournalEntryComponent(entries)
+    }
+}
+
+renderJournalEntries(journalEntry)
+
+
+console.log(journalEntry)
